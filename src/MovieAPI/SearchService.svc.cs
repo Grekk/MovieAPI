5b99d8c439cfbb596fba4704583f0582b1a73cd7
Gregory Maynard <gregory.maynard@valtech.fr>
2012-07-26 18:14:32 +0200

    Add searchservice ref

---
+++ src/MovieAPI/SearchService.svc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using MoviesRememberDomain;
+using MoviesRememberServices.Utils;
+
+namespace MovieAPI
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "SearchService" in code, svc and config file together.
+    public class SearchService : ISearchService
+    {
+        private readonly ISearchService _searchService;
+
+        public SearchService()
+        {
+            _searchService = Bootstrapper.GetInstance<ISearchService>();
+        }
+
+        public TinyMovieList Search(string query)
+        {
+            return _searchService.Search(query);
+        }
+    }
+}
