2eb23e94ffe5a86f71fccf1721b615c3a8473714
Gregory Maynard <gregory.maynard@valtech.fr>
2012-07-26 12:34:18 +0200

    Fix build

--- src/MoviesRememberDao/UserMovieDAO.cs
+++ src/MoviesRememberDao/UserMovieDAO.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using MoviesRememberDomain;
 using MoviesRememberDao.Interface;
-using AutoMapper;
 using MoviesRememberDB;
 
 namespace MoviesRememberDao
