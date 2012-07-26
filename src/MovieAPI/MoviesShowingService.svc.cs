f2ea80c3d066f0cb719e250af92017f17439b9bb
Gregory Maynard <gregory.maynard@valtech.fr>
2012-07-26 11:58:30 +0200

    ignore resharper

---
+++ src/MovieAPI/MoviesShowingService.svc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using MoviesRememberDomain;
+using MoviesRememberServices.Utils;
+using StructureMap;
+
+namespace MovieAPI
+{
+    public class MoviesShowingService : IMoviesShowingService
+    {
+        private readonly MoviesRememberServices.Interface.IMoviesShowingService _moviesShowingService;
+
+        public MoviesShowingService()
+        {
+            new Bootstrapper().Bootstrap();
+            _moviesShowingService = ObjectFactory.GetInstance<MoviesRememberServices.Interface.IMoviesShowingService>();
+        }
+
+        public TinyMovieList GetNowShowingMoviesByRate(int numPage)
+        {
+            return _moviesShowingService.GetNowShowingMoviesByRate(numPage);
+        }
+
+        public TinyMovieList GetNowShowingMoviesByDate(int numPage)
+        {
+            return _moviesShowingService.GetNowShowingMoviesByDate(numPage);
+        }
+
+        public TinyMovieList GetComingSoonMoviesByRate(int numPage)
+        {
+            return _moviesShowingService.GetComingSoonMoviesByRate(numPage);
+        }
+
+        public TinyMovieList GetComingSoonMoviesByDate(int numPage)
+        {
+            return _moviesShowingService.GetComingSoonMoviesByDate(numPage);
+        }
+
+        public IList<TinyMovie> GetBestWeekMovies()
+        {
+            return _moviesShowingService.GetBestWeekMovies();
+        }
+
+        public Movie GetMovie(long code)
+        {
+            return _moviesShowingService.GetMovie(code);
+        }
+    }
+}
