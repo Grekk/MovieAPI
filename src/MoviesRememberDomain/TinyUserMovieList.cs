8d8d18631efcc483963d67f32baf940f42a294cc
Gregory Maynard <gregory.maynard@valtech.fr>
2012-08-17 13:49:15 +0200

    add file tinyusermovie list

---
+++ src/MoviesRememberDomain/TinyUserMovieList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace MoviesRememberDomain
+{
+    [DataContract]
+    public class TinyUserMovieList
+    {
+        public TinyUserMovieList()
+        {
+            TinyUserMovies = new PagedList<UserMovie>();
+        }
+
+        [DataMember]
+        public PagedList<UserMovie> TinyUserMovies { get; set; }
+    }
+
+}
